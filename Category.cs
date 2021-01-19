aa3c2960a0d86216bdb476f1638ddb71e2dd3b0d
mercymastermind <reubennita16@gmail.com>
2021-01-19 16:05:21 +0100

    Added more class functionality

---
+++ Category.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Black_Friday
+{
+    class Category
+    {
+        string name;
+        Array productNames;
+        Array productPrices;
+
+        private Category() { }
+
+        public static Category createInstance(string name, string[] productsNameList, double[] productsPriceList, int size)
+        {
+            Category temp = new Category();
+            temp.setName(name);
+            temp.setProductNames(productsNameList, size);
+            temp.setProductPrices(productsPriceList, size);
+
+            return temp;
+        }
+
+        private void setName(string categoryName)
+        {
+            name = categoryName;
+        }
+
+        private void setProductNames(string[] productNamesList, int size)
+        {
+            productNames = Array.CreateInstance(typeof(string), size);
+            for(int i = 0; i < size; i++)
+            {
+                productNames.SetValue(productNamesList[i], i);
+            }
+        }
+
+        private void setProductPrices(double[] productPriceList, int size)
+        { 
+            productPrices = Array.CreateInstance(typeof(double), size);
+            for (int i = 0; i < size; i++)
+            {
+                productPrices.SetValue(productPriceList[i], i);
+            }
+        }
+
+        public string getName() => name;
+
+        public Array getProductNames()
+        {
+            return productNames;
+        }
+        public Array getProductPrices => productPrices;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,17 +5,46 @@ namespace Black_Friday
     class BlackFiday
     {
         DayOfWeek friday = DayOfWeek.Friday;
-        static DateTime staticDateTime = new DateTime(2021, 01, 15);
+        static DateTime staticDateTime = DateTime.Now;
         DateTime currentdt = staticDateTime;
-        string[] Clothing_AccessoryList = new string[10] { "Blue Denim Jacket", "Ferragamo Slip-In Loafers", "Louboutini Red Bottom Heels", "Two-sided Bucket Hats", "Nike Off The Wall", "Brown Fenty Purse", "Fashion Winter Boots", "Off White Winter Coat", "Beluga Necklace", "Men Thick Cuban Chain" };
-        double[] Clothing_AccessoryOriginalPriceList = new double[10] { 100, 2000, 4000, 50, 150, 500, 350, 1050, 900, 300 };
-        string[] electronicsList = new string[10] { "Samsung A20", "LG 120 inch Solar TV", "Higher Thermocool Electronic Washing Machine", "HP Polaroid 15 inch Gaming Laptop", "Iphone 12 Pro", "Soundless Maxi Generator", "Iphone Earpods", "Tecno Camon 12", "HD Stereo Palma Set", "Touch Sensitive Light Box" };
-        double[] electronicsOriginalPriceList = new double[10] { 1000, 2500, 400, 5050, 200, 500, 350, 450, 70, 120 };
-        string[] automobile = new string[10] { "Lexus 320 (2020 Model)", "Mercedes Benz X200", "Honda Accord", "Porsche Spare Tyres", "Masarati Engine Oil", "Lexus 470 HeadLamps", "Toyota Corolla 2019", "Mack Truck Spare Tyres", "Lambourghini Limited 2020 Edition", "Car body spray" };
-        double[] automobileOriginalPriceList = new double[10] { 10000, 25000, 4000, 5900, 2000, 8000, 3000, 3000, 500000, 400 };
-        string[] foodHousehold = new string[10] { "100KG Bag Of Rice", "Carton Of Tasty Tom Tin Tomatoes", "Trailler of Coke", "10 Gallons of Power Oil", "Marbe DinnerWare Set", "Gold Crested Ice cream Cups & Spoons", "Kellog's Corn Flakes", "Skippy Peanut Butter", "Golden Penny Spaghetti", "Golden Morn" };
-        double[] foodHouseholdOriginalPriceList = new double[10] { 1000, 2000, 400, 500, 200, 800, 300, 300, 5000, 400 };
+        string[] CLOTHING_ACCESSORIES = new string[10] { "Blue Denim Jacket", "Ferragamo Slip-In Loafers", "Louboutini Red Bottom Heels", "Two-sided Bucket Hats", "Nike Off The Wall", "Brown Fenty Purse", "Fashion Winter Boots", "Off White Winter Coat", "Beluga Necklace", "Men Thick Cuban Chain" };
+        double[] CLOTHING_ACCESSORIES_PRICES = new double[10] { 100, 2000, 4000, 50, 150, 500, 350, 1050, 900, 300 };
+        string[] ELECTRONICS = new string[10] { "Samsung A20", "LG 120 inch Solar TV", "Higher Thermocool Electronic Washing Machine", "HP Polaroid 15 inch Gaming Laptop", "Iphone 12 Pro", "Soundless Maxi Generator", "Iphone Earpods", "Tecno Camon 12", "HD Stereo Palma Set", "Touch Sensitive Light Box" };
+        double[] ELECTRONICS_PRICES = new double[10] { 1000, 2500, 400, 5050, 200, 500, 350, 450, 70, 120 };
+        string[] AUTOMOBILE = new string[10] { "Lexus 320 (2020 Model)", "Mercedes Benz X200", "Honda Accord", "Porsche Spare Tyres", "Masarati Engine Oil", "Lexus 470 HeadLamps", "Toyota Corolla 2019", "Mack Truck Spare Tyres", "Lambourghini Limited 2020 Edition", "Car body spray" };
+        double[] AUTOMOBILE_PRICES = new double[10] { 10000, 25000, 4000, 5900, 2000, 8000, 3000, 3000, 500000, 400 };
+        string[] FOOD_HOUSEHOLD = new string[10] { "100KG Bag Of Rice", "Carton Of Tasty Tom Tin Tomatoes", "Trailler of Coke", "10 Gallons of Power Oil", "Marbe DinnerWare Set", "Gold Crested Ice cream Cups & Spoons", "Kellog's Corn Flakes", "Skippy Peanut Butter", "Golden Penny Spaghetti", "Golden Morn" };
+        double[] FOOD_HOUSEHOLD_PRICES = new double[10] { 1000, 2000, 400, 500, 200, 800, 300, 300, 5000, 400 };
         DateTime[] allFridays = new DateTime[5];
+        static Array allCategories = Array.CreateInstance(typeof(Category), 4);
+
+        private void getFridays()
+        {
+            int numDays = DateTime.DaysInMonth(staticDateTime.Year, staticDateTime.Month);
+            int index = 0;
+            DateTime newDay = new DateTime(staticDateTime.Year, staticDateTime.Month, 01);
+            while (numDays > 0)
+            {
+                if (newDay.DayOfWeek == friday)
+                {
+                    allFridays[index] = newDay;
+                    index++;
+                }
+                newDay = newDay.AddDays(1);
+                numDays--;
+            }
+        }
+
+        private void setUp()
+        {
+            allCategories.SetValue(Category.createInstance("Clothing and Accessories", CLOTHING_ACCESSORIES, CLOTHING_ACCESSORIES_PRICES, 10), 0);
+            allCategories.SetValue(Category.createInstance("Electronics", ELECTRONICS, ELECTRONICS_PRICES, 10), 1);
+            allCategories.SetValue(Category.createInstance("Automobile", AUTOMOBILE, AUTOMOBILE_PRICES, 10), 2);
+            allCategories.SetValue(Category.createInstance("Food and Household", CLOTHING_ACCESSORIES, CLOTHING_ACCESSORIES_PRICES, 10), 3);
+        }
+
+        
+
         public enum fridaysOfTheMonth
         {
             firstFriday, secondFriday, thirdFriday, lastFriday
@@ -56,25 +85,8 @@ namespace Black_Friday
         {
             Console.WriteLine("Welcome To the biggest sales in Africa, Black Friday!");
             Console.WriteLine("Below are the lists of the black fridays and the categories of products with discounts on those days");
-            getFridays();
+            setUp();
             optionList();
-        }
-
-        public void getFridays()
-        {
-            int numDays = DateTime.DaysInMonth(staticDateTime.Year, staticDateTime.Month);
-            int index = 0;
-            DateTime newDay = new DateTime(staticDateTime.Year, staticDateTime.Month, 01);
-            while (numDays > 0)
-            {
-                if(newDay.DayOfWeek == friday)
-                {
-                    allFridays[index] = newDay;
-                    index++;
-                }
-                newDay = newDay.AddDays(1);
-                numDays--;
-            }
         }
 
         public void optionList()
@@ -88,37 +100,29 @@ namespace Black_Friday
                 "Enter 2 for Electronics\n" +
                 "Enter 3 for Automobiles\n" +
                 "Enter 4 for Food & Household Items");
-            option = Convert.ToInt16(Console.ReadLine());
-            optionChoice();
+            int userChoice = Convert.ToInt16(Console.ReadLine());
+            optionChoice(userChoice);
         }
 
-        public void optionChoice()
+        public void optionChoice(int userChoice)
         {
-            switch (option)
+            Category temp;
+
+            if(userChoice > 0 && userChoice <= 4)
             {
-                case 1:
-                    Console.WriteLine("Welcome to the Clothing and Acessories Category, we have great deals for you!");
-                    categoryChoice(Clothing_AccessoryList, Clothing_AccessoryOriginalPriceList);
-                    break;
-
-                case 2:
-                    Console.WriteLine("Welcome to the Electronics Category, we have great deals for you!");
-                    categoryChoice(electronicsList, electronicsOriginalPriceList);
-                    break;
-
-                case 3:
-                    Console.WriteLine("Welcome to the Automobile, we have great deals for you!");
-                    categoryChoice(automobile, automobileOriginalPriceList);
-                    break;
-                case 4:
-                    Console.WriteLine("Welcome to the Food and Household Items Category, we have great deals for you!");
-                    categoryChoice(foodHousehold, foodHouseholdOriginalPriceList);
-                    break;
-                default:
-                    Console.WriteLine("Please choose a category between 1-4 ");
-                    optionList();
-                    break;
+                temp = (Category)allCategories.GetValue(userChoice - 1);
+                Console.WriteLine($"Welcome to the {temp.getName()} Category, we have great deals for you!");
+                categoryChoice(temp);
             }
+            
+            else
+            {
+                Console.WriteLine("Invalid choice! Please try again!");
+                optionList();
+            }
+
+
+            
         }
         public double getDiscount(fridaysOfTheMonth dt)
         {
@@ -153,7 +157,7 @@ namespace Black_Friday
             }
             return upperFriday;
         }
-        public void categoryChoice(String[] productList, double[] productPriceList)
+        public void categoryChoice(Category cat)
         {
             string strDiscount;
 
@@ -181,11 +185,17 @@ namespace Black_Friday
                 Console.WriteLine("All Products in this category are at a discounted price of {0}%(Black Friday Deals!!!)", strDiscount);
             }
 
-            while (i < productList.Length)
+            Array categoryProductNames = cat.getProductNames();
+
+            while (i < categoryProductNames.Length)
             {
-                double calculatedDiscount = (productPriceList[i] - productPriceList[i] * discount);
-                Console.WriteLine(productList[i] + "\nPrice -> " + productPriceList[i] + "$ \nDiscounted Price ->" + 
-                    calculatedDiscount + "$ \n");
+                double calculatedDiscount = (categoryProductNames.GetValue(i) - productPriceList[i] * discount);
+                //Console.WriteLine(productList[i] + "\nPrice -> {}", productPriceList[i]);
+                Console.WriteLine($"{productList[i]} \nPrice {productPriceList[i]}");
+                if (discount != 0)
+                {
+                    Console.Write($"\nDiscounted Price -> {calculatedDiscount * productPriceList[i]}");
+                }
                 i++;
             }
         }
